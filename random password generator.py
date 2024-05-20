1524a139b0334e87a5dbd50a32df7979fce06196
klnsrivalli <139358622+klnsrivalli@users.noreply.github.com>
2024-05-20 17:18:52 +0530

    Add files via upload

---
+++ random password generator.py
@@ -0,0 +1,21 @@
+import random #random module
+import string #string module provides collection of constants
+
+print("****RANDOM PASWORD GENEATOR****")
+
+#user prompt to take input of desired passsword_length
+password_length = int(input("Enter the password length: "))
+
+#string variable that contains all letters,digits and punctuations
+characters = string.ascii_letters + string.digits + string.punctuation
+
+#initialised empty string
+password = ""   
+
+for index in range(password_length):
+    
+    #randomly chooses characters from character string
+    password = password + random.choice(characters)
+
+# Displays password
+print(" Random Password generated: {}".format(password))
